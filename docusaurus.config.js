a8c5286b9d36c6fc2f596200e632e2d22151ca3c
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-02-13 19:36:16 -0300

    fix: add correct title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 
 const config = {
-  title: "Ionic Documentation",
+  title: "Docs",
   tagline: "Dinosaurs are cool",
   url: "https://mauriciomutte.dev",
   baseUrl: `/`,
